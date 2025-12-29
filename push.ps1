089ec6dfd9b565d3ace5c8ced8e52721c3caf4ed
randomusert <127417963+randomusert@users.noreply.github.com>
2025-12-29 11:49:08 +0200

    Fix echo statements in push.ps1
    Updated echo statements for improved clarity. doesn't print as new lines with a space. This a quirk in powershell's echo since  it is renamed Write-Output
    
    Signed-off-by: randomusert <127417963+randomusert@users.noreply.github.com>

--- push.ps1
+++ push.ps1
@@ -1,8 +1,8 @@
-echo pushing to github
+echo "pushing to github"
 git push -u origin 1.21.1
 
-echo pushing to gitlab
+echo "pushing to gitlab"
 git push --all gitlab
 git push --tags gitlab
-echo complete
+echo "Finnished pushing to github and gitlab. if any errors occured from git, check them and solve"
 pause
