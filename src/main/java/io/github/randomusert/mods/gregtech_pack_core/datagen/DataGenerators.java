433565b4e9ae91e6eb1adbb6410a27345d7eb04e
randomusert <eetupajamaki@gmail.com>
2025-12-31 13:47:44 +0200

    fix datagen recipe provider. recipe provider was not even registered on DataGenerators.java

--- src/main/java/io/github/randomusert/mods/gregtech_pack_core/datagen/DataGenerators.java
+++ src/main/java/io/github/randomusert/mods/gregtech_pack_core/datagen/DataGenerators.java
@@ -34,6 +34,8 @@ public class DataGenerators {
                 List.of(new LootTableProvider.SubProviderEntry(ModBlockLootTableProvider::new, LootContextParamSets.BLOCK)), lookupProvider));
 
 
+
+        generator.addProvider(event.includeServer(), new ModRecipeProvider(packOutput, lookupProvider));
         BlockTagsProvider blockTagsProvider = new ModBlockTagProvider(packOutput, lookupProvider, existingFileHelper);
         generator.addProvider(event.includeServer(), blockTagsProvider);
 
